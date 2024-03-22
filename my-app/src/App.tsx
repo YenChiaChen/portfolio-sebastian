19f7e5b5300fb3195ff86384b3fa47db5e3e6df3
Yen-Chia Chen <107033180+YenChiaChen@users.noreply.github.com>
2024-03-22 13:03:42 +0800

    feat: add header component

--- my-app/src/App.tsx
+++ my-app/src/App.tsx
@@ -1,10 +1,9 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import LandingPage from "./Pages/LandingPage";
 
 function App() {
   return (
-    <button className="btn">Button</button>
+    <LandingPage />
+
   );
 }
 
---
+++ my-app/src/Pages/LandingPage/index.tsx
@@ -0,0 +1,14 @@
+import Header from "../../Components/Header";
+import TextSwap from "../../Components/Component/TestSwap";
+
+
+export default function LandingPage() {
+    return (
+        <>
+            <Header />
+            <div className='h-[100vh] bg-orange-500'></div>
+            <div className='h-[200vh]'></div>
+        </>
+    );
+  }
+  
