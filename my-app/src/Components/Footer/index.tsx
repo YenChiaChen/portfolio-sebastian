1fd340b573cde979ab098c6e689d88f346be759b
Yen-Chia Chen <107033180+YenChiaChen@users.noreply.github.com>
2024-03-23 09:41:18 +0800

    update: project footer

--- my-app/src/Components/Footer/index.tsx
+++ my-app/src/Components/Footer/index.tsx
@@ -4,24 +4,28 @@ export default function Footer() {
     return (
         <footer className="footer p-10 bg-gray-300 text-neutral">
           <nav>
-            <h6 className="footer-title">Services</h6> 
-            <a className="link link-hover">Branding</a>
-            <a className="link link-hover">Design</a>
-            <a className="link link-hover">Marketing</a>
-            <a className="link link-hover">Advertisement</a>
+            <h6 className="footer-title">DATE</h6> 
+            <a className=" ">2023</a>
+            <a className=" ">5-Month Individual Project</a>
           </nav> 
           <nav>
-            <h6 className="footer-title">Company</h6> 
-            <a className="link link-hover">About us</a>
-            <a className="link link-hover">Contact</a>
-            <a className="link link-hover">Jobs</a>
-            <a className="link link-hover">Press kit</a>
+            <h6 className="footer-title">ROLE</h6> 
+            <a className=" ">Service Design</a>
+            <a className=" ">UI / UX</a>
+            <a className=" ">Operational Strategy</a>
           </nav> 
           <nav>
-            <h6 className="footer-title">Legal</h6> 
-            <a className="link link-hover">Terms of use</a>
-            <a className="link link-hover">Privacy policy</a>
-            <a className="link link-hover">Cookie policy</a>
+            <h6 className="footer-title">ORGANIZATION</h6> 
+            <a className="  font-bold">Collaborators</a>
+            <a className=" ">The Neurodivisity Charity</a>
+            <a className=" ">The Brain Charity</a>
+          </nav>
+          <nav>
+            <h6 className="footer-title">OUTCOMES</h6> 
+            <a className=" ">A Google Chrome Extension,</a>
+            <a className=" ">ADHD Financial Learning Journey Map,</a>
+            <a className=" ">Service Blueprint and System Map for ADHDy</a>
+            <a className=" ">Financial Course</a>
           </nav>
         </footer>
     );
