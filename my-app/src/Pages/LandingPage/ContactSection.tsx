70761e3417fbf3b531ed468f1bbf16acc12aaac0
yenchiachen <charlisyenchiachen@gmail.com>
2024-04-07 21:43:47 +0800

    style: contact content widfh

--- my-app/src/Pages/LandingPage/ContactSection.tsx
+++ my-app/src/Pages/LandingPage/ContactSection.tsx
@@ -6,7 +6,7 @@ export default function ContactSection() {
                 <div className='triangle absolute left-0 top-0'></div>
                 <div className='container mx-auto py-12 text-center'>
                    <p className="text-[36px] font-bold text-[#DD663C] tracking-[16px]">CONTACT</p>
-                   <p className='text-left w-[650px] mx-auto text-[#D9D9D9] mt-4 tracking-widest font-light text-[20px] mb-4'>For inquiries or opportunities, feel free to reach out to me! I'm always open to connecting with fellow professionals and discussing design. <span className='font-bold'>Let's spin impactful services together!</span></p>
+                   <p className='text-left max-w-[660px] mx-auto text-[#D9D9D9] mt-4 tracking-widest font-light text-[20px] mb-4'>For inquiries or opportunities, feel free to reach out to me! I'm always open to connecting with fellow professionals and discussing design. <span className='font-bold'>Let's spin impactful services together!</span></p>
                 </div>
             </div>
             <div className='bg-[#202020] h-[60px]'></div>
